88d51baea2bc0ac79237094814ff551955cdc105
Volodymyr Mostepaniuk <mostepaniukvm@gmail.com>
2020-12-17 14:55:05 +0200

    DEV-1786: Added possible changes from 6.x branch.

--- scripts/init-devsetup-docker.sh
+++ scripts/init-devsetup-docker.sh
@@ -9,18 +9,18 @@ if [[ $PROJECT_ADD_DEVSETUP_DOCKER = 1 ]]; then
   echo "Adding devsetup-docker from https://github.com/drunomics/devsetup-docker..."
   echo "Set PROJECT_ADD_DEVSETUP_DOCKER=0 to disable."
 
-  git clone https://github.com/drunomics/devsetup-docker.git --branch=2.x devsetup-tmp
+  git clone https://github.com/drunomics/devsetup-docker.git --branch=3.x devsetup-tmp
   rm -rf devsetup-tmp/.git devsetup-tmp/README.md
-  
+
   # OS specific cp operations
   case "$OSTYPE" in
-    darwin*) 
-      if [  -d "./devsetup-tmp" ] 
+    darwin*)
+      if [  -d "./devsetup-tmp" ]
       then
-        cp -rf devsetup-tmp/* . 
+        cp -rf devsetup-tmp/* .
         cp -rf devsetup-tmp/.[^.]* .
-      fi ;; 
-    linux*)  cp -rfT devsetup-tmp . ;; 
+      fi ;;
+    linux*)  cp -rfT devsetup-tmp . ;;
     *)       cp -rfT devsetup-tmp . ;;
   esac
 
@@ -28,7 +28,7 @@ if [[ $PROJECT_ADD_DEVSETUP_DOCKER = 1 ]]; then
   php process-replacements.php
   rm -rf devsetup-tmp process-replacements.php
   echo \
-'COMPOSE_AMAZEEIO_VERSION=v0.22.1
-COMPOSE_AMAZEEIO_PHP_VERSION=7.2
+'COMPOSE_AMAZEEIO_VERSION=v1.9.1
+COMPOSE_AMAZEEIO_PHP_VERSION=7.4
 ' >> .env-defaults
 fi
